69cc4f334b0fdd42e462f1b8a1c95654bfd07876
Sumeru Gowda <155037091+sumeru-26@users.noreply.github.com>
2024-03-19 10:58:31 -0700

    added env var for db access

--- mongodb.py
+++ mongodb.py
@@ -1,7 +1,11 @@
+import os
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-from user import user,password
+#from user import user,password
+user = os.environ["USER"]
+password = os.environ["PASSWORD"]
 
 uri = f"mongodb+srv://{user}:{password}@openscouting.xsr04sk.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
