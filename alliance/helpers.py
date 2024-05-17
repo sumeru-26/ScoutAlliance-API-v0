189e7cc7f5be3ee8040b71740e4eacad23f336df
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-05-16 17:26:24 -0700

    added alliance additions

--- alliance/helpers.py
+++ alliance/helpers.py
@@ -1,14 +1,29 @@
 from mongodb import alliances_db
 
+alliance_cache = {}
 access_cache = {}
 
 def get_access(team: int):
     if team not in access_cache:
         cache_access(team)
-    return access_cache[team]
+    teams = []
+    for alliance in access_cache[team]:
+        teams.extend(alliance_cache[alliance])
+    return teams
 
 def cache_access(team: int):
-    data = alliances_db.find_one({"teams" : team},{"_id" : 0})
+    data = alliances_db.find({"teams" : team}, {"_id" : 0})
     if data is None:
         raise ValueError("Team does not exist")
-    access_cache[team] = data["teams"]
+    if team not in access_cache.keys():
+        access_cache[team] = []
+    for x in data:
+        alliance_cache[x["name"]] = x["teams"]
+        access_cache[team].append(x["name"])
+
+def add_access(team: int, alliance_name: str):
+    alliances_db.update_one({"name": alliance_name},
+                            {"$push": {
+                                "teams": team
+                            }})
+    cache_access(team)
--- alliance/router.py
+++ alliance/router.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter,Depends
 
-from .helpers import get_access
+from .helpers import get_access,add_access
 from auth import get_user
 
 allianceRouter = APIRouter()
 
-@allianceRouter.get("/test")
-async def test(
+@allianceRouter.get("/get")
+async def get(
     team_number: int = Depends(get_user)):
     return get_access(team_number)
+
+@allianceRouter.post("/add")
+async def add(
+    team: int,
+    alliance: str,
+    user: int = Depends(get_user)):
+    if user == 9999:
+        add_access(team, alliance)
