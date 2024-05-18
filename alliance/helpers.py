cb64a9ec93ae992533f72476687609adb55dbd11
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-05-18 09:47:37 -0700

    added alliance removing + fixed caching

--- alliance/helpers.py
+++ alliance/helpers.py
@@ -1,6 +1,10 @@
 from mongodb import alliances_db
 
+from pprint import pprint
+
+# stores teams on alliance e.g. { "test-alliance": [254, 1678, 4414] }
 alliance_cache = {}
+# stores alliances that team is on e.g. { 2374: ["alliance1", "alliance2", "alliance3"] }
 access_cache = {}
 
 def get_access(team: int):
@@ -19,11 +23,29 @@ def cache_access(team: int):
         access_cache[team] = []
     for x in data:
         alliance_cache[x["name"]] = x["teams"]
-        access_cache[team].append(x["name"])
+        access_cache[team].append(x["name"]) 
+    pprint(alliance_cache)
+    pprint(access_cache)
+
+def update_access_locally(team: int, alliance: str, remove: bool = False):
+    if team not in access_cache.keys():
+        cache_access(team)
+    alliance_cache[alliance].remove(team) if remove else alliance_cache[alliance].append(team)
+    access_cache[team].remove(alliance) if remove else access_cache[team].append(alliance)
+    pprint(alliance_cache)
+    pprint(access_cache)
 
 def add_access(team: int, alliance_name: str):
     alliances_db.update_one({"name": alliance_name},
                             {"$push": {
                                 "teams": team
                             }})
-    cache_access(team)
+    update_access_locally(team, alliance_name)
+    
+
+def remove_access(team: int, alliance_name: str):
+    alliances_db.update_one({"name": alliance_name},
+                            {"$pull": {
+                                "teams": team
+                            }})
+    update_access_locally(team, alliance_name, remove=True)
--- alliance/router.py
+++ alliance/router.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter,Depends
 
-from .helpers import get_access,add_access
+from .helpers import get_access,add_access,remove_access
 from auth import get_user
 
 allianceRouter = APIRouter()
 
 @allianceRouter.get("/get")
 async def get(
+    team_override: int = None,
     team_number: int = Depends(get_user)):
+    if (team_number == 9999 and team_override is not None):
+        return get_access(team_override)
     return get_access(team_number)
 
 @allianceRouter.post("/add")
@@ -17,3 +20,11 @@ async def add(
     user: int = Depends(get_user)):
     if user == 9999:
         add_access(team, alliance)
+
+@allianceRouter.delete("/remove")
+async def remove(
+    team: int,
+    alliance: str,
+    user: int = Depends(get_user)):
+    if user == 9999:
+        remove_access(team, alliance)
